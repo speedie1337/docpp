8d3e23ac5a7411f6634dc713dd1d33adb5011f20
speedie <speedie@speedie.site>
2024-05-17 08:41:14 +0200

    Use TYPE_TEXT_TAB instead for ELEMENT_EMPTY

--- src/docpp.cpp
+++ src/docpp.cpp
@@ -291,7 +291,7 @@ void docpp::HTML::Section::set(const std::string& tag, const Properties& propert
 
 std::pair<std::string, docpp::HTML::Type> docpp::HTML::resolve_tag(const Tag tag) {
     static const std::unordered_map<Tag, std::pair<std::string, docpp::HTML::Type>> tag_map{
-        {ELEMENT_EMPTY, {"", docpp::HTML::TYPE_TEXT}},
+        {ELEMENT_EMPTY, {"", docpp::HTML::TYPE_TEXT_TAB}},
         {ELEMENT_ABBREVIATION, {"abbr", docpp::HTML::TYPE_NON_SELF_CLOSING}},
         {ELEMENT_ABBR, {"abbr", docpp::HTML::TYPE_NON_SELF_CLOSING}},
         {ELEMENT_ACRONYM, {"acronym", docpp::HTML::TYPE_NON_SELF_CLOSING}},
